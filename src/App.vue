ae4b26808c28a5f17b02809ddd5276706ebac8f5
“Alexbarile” <alexbarile93fg@yahoo.it>
2023-01-13 16:07:24 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppCards from './components/AppCards.vue';
+
+export default {
+  components: {
+    AppHeader,
+    AppCards,
+  }
+}
+
+</script>
+
+<template>
+  <AppHeader></AppHeader>
+  <AppCards></AppCards>
+  
+</template>
+
+<style lang="scss">
+@use './styles/generals.scss' as *;
+
+</style>
---
+++ src/components/AppCards.vue
@@ -0,0 +1,22 @@
+<script>
+import {store} from '../store.js'
+export default {
+    data() {
+        return {
+            store
+        }
+    },
+    
+}
+</script>
+
+<template>
+    <div>
+        {{ store }}
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/mixins' as *;
+@use '../styles/partials/variables' as *;
+</style>
